b6caf17147b29679034f4f096415a7f2c7314131
Валентин <27902270+vdistortion@users.noreply.github.com>
2025-05-17 20:41:42 +0500

    refactor api/set-webhook.ts

--- api/set-webhook.ts
+++ api/set-webhook.ts
@@ -1,15 +1,15 @@
-import { Bot } from 'grammy';
 import type { VercelRequest, VercelResponse } from '@vercel/node';
+import { bot } from '../src';
 
 const VERCEL_URL = process.env.VERCEL_URL;
-const bot = new Bot(process.env.TELEGRAM_BOT_TOKEN!);
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
   try {
     if (!VERCEL_URL) return res.status(401).json({ message: 'Unauthorized' });
     const webhookUrl = `https://${VERCEL_URL}/api`;
+    const url = `https://api.telegram.org/bot${bot.api.token}/setWebhook?url=${webhookUrl}`;
     await bot.api.setWebhook(webhookUrl);
-    res.status(200).json({ message: `Webhook установлен на ${webhookUrl}` });
+    res.status(200).json({ message: `Webhook установлен на ${webhookUrl}`, url });
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: 'Ошибка установки webhook' });

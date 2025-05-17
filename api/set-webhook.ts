a17701dfb1aee2a9f17a589e4667d3d41ce9aa45
Валентин <27902270+vdistortion@users.noreply.github.com>
2025-05-17 12:38:37 +0500

    setWebhook refactor 2

--- api/set-webhook.ts
+++ api/set-webhook.ts
@@ -3,15 +3,17 @@ import { Bot } from 'grammy';
 const TELEGRAM_BOT_TOKEN = process.env.TELEGRAM_BOT_TOKEN;
 const VERCEL_URL = process.env.VERCEL_URL;
 
-if (!TELEGRAM_BOT_TOKEN || !VERCEL_URL)
+if (!TELEGRAM_BOT_TOKEN || !VERCEL_URL) {
   throw new Error('TELEGRAM_BOT_TOKEN или VERCEL_URL не установлены');
+}
 
 const bot = new Bot(TELEGRAM_BOT_TOKEN);
 const webhookUrl = `https://${VERCEL_URL}/api`;
-console.log({ webhookUrl, TELEGRAM_BOT_TOKEN });
 
 export default async function handler() {
-  await bot.api.setWebhook(webhookUrl).then(() => `Webhook установлен на ${webhookUrl}`);
+  await bot.api
+    .setWebhook(webhookUrl)
+    .then(() => new Response(`Webhook установлен на ${webhookUrl}`))
+    .catch(console.error)
+    .finally(() => console.log({ webhookUrl, TELEGRAM_BOT_TOKEN }));
 }
-
-handler().then(console.info);
